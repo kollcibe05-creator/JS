d33dda57996d9c5a167978ceb1c6ccf424e96852
Collins Kibet <kollcibe05@gmail.com>
2026-03-20 07:20:10 +0300

    Plato, aristotle, syntactic sugar and OOP

---
+++ OOP/introToprototypalInheritance.js
@@ -0,0 +1,43 @@
+//With OOP we've learnt, we have used the syntax introduced in around 2014.
+//This syntax uses the class keyword and methods called constructors() to initialize new instances of the class. We'll call this the 'ES2015 standard'
+//'ES2015 standard' is similar to Ruby, Python, and Java and feels to many devs learning JS as a second language to be 'the way OO should be done.'
+
+                                                    //***********DEFINE `syntactic sugar`**************//
+//From JS's perspective, ES2015 syntax is "syntactic sugar".                                                     
+//This is a term that developers use to communicate "we made the thing easier to type and to read, but underneath, it's still doing something a lot more complicated."
+//The OO model that ES2015 simplifies is the 'prototypal object oriented' model that JS actually works in.
+                                                   //********Recognize that the inheritance Model od JS is Prototypal*******//     
+//IMPORTANT: This is a common interview question used to weed-out front end developers. When asked what kind of inheritance model JavaScript has, boldly and proudly say "Prototypal!" To demonstrate mastery of this vocabulary word, you'll need to do the rest of the work in this section, though. :)                                                   
+//The difference between Prototypal and Class-based OO goes all the way back to Plato and Aristotle. No kidding! 
+
+//In Plato's Theory of Forms he suggests that when we say "tree", our minds build a foggy rough idea of what all trees are like. 
+//Philosophy students call this "the form of a Tree" or "Tree-ness."
+//Philosophy students call this "the form of a Tree" or "Tree-ness."
+//My conception of "Tree-ness" has something to do with bark, and leaves, and green, and pine trees, and oak trees. Yours is probably similar.
+//When we find or create a specific instance that "participates in" or "references" a form it stops being a cloudy murky idea and becomes a real thing. Not just Dog-ness, but my Maria, Bosko.
+//This should remind you of the object-oriented world of Ruby, Java, Python, and ES2015 standard JavaScript.
+//Forms are classes and instances are the new embodiments of those classes with some unique, specific, real data bonded to them (via a constructor).
+
+//ASIDE: Programming, for many, is an exercise in applied philosophy.
+
+//Aristotle, the biologically-minded student of Plato's, said that his teacher was talking about make-believe nonsense like dragons and unicorns when he talked about "forms."
+//No one has ever seen a Tree-ness or a Dog-ness.
+//Everything that is, Aristotle argued, is like a something else with more specification added.
+//Everything new is based off of a pre-existing pattern, a first of its kind, the proto(first) - typos(kind), or prototype.
+    //A Poodle is based off the Water-Dog breed...
+    //...and Water-Dogs are breeds based off of Dogs...
+    //...and Dogs are domesticated Wolves...
+    //...and a wolf is a carnivorous mammal...
+    //...which means it's a chordate and all chordates are animals (not plants).
+//This probably reminds you of basic biology and the KPCOFGS way of naming the things living on this planet.
+        //Kingdom > Phylum > Class > Order > Family > Genus > Species
+        //Animalia > Chordata > Mammalia > Carnivora > Canidae > Canis > lupus
+//Given this view of the world, it's no surprise Aristotle was a huge fan of making trees 
+    // (another computer science idea that started in philosophy) 
+    // of these relationships (or, "taxonomies," another computer science idea that started in philosophy).        
+//Languages that allow you to "extend what's already there"; that is, ```Prototypal object systems``` are languages like ``````````C, Lisp, Self, and JavaScript (natively)````````.
+//Ultimately, both class-based (or, "form-based") and prototype-based models are ways of describing how to create instances from a common ancestor. 
+// Neither is "better" than the other. They're just different ways of seeing objects (no pun intended) in the world.
+
+
+
